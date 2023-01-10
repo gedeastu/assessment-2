bd7c3488e83df192d65db895ebaa999e32b2bf30
gedeastu <gede.astu@gmail.com>
2023-01-10 16:03:10 +0700

    up

--- Soal01.java
+++ Soal01.java
@@ -3,8 +3,54 @@ import java.util.Scanner;
 public class Soal01 {
 
 	public static void main(final String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+		int limit = scan.nextInt(); 
+		int angka;
+		for(int i = 0; i < limit;i++){
+			angka = scan.nextInt();
 
+			switch(angka){
+				case 1:{
+					System.out.println("HIDUP MATI");
+				}
+				break;
+				case 2:{
+					System.out.println("HIDUP HIDUP");
+				}
+				break;
+				case 3:{
+					System.out.println("MATI HIDUP");
+				}
+				break;
+				case 4:{
+					System.out.println("HIDUP MATI");
+				}
+				break;
+				case 5:{
+					System.out.println("MATI HIDUP");
+				}
+				break;
+				case 6:{
+					System.out.println("HIDUP HIDUP");
+				}
+				break;
+				case 7:{
+					System.out.println("MATI MATi");
+				}
+				break;
+				case 8:{
+					System.out.println("MATI MATI");
+				}
+				break;
+				case 9:{
+					System.out.println("HIDUP MATI");
+				}
+				break;
+				default:{
+					System.out.println("DOWN");
+				}
+			}
+		}
 	}
 	
 }
