cc915a044f3a3fb704ce9db00aa368bbbf307aee
gedeastu <gede.astu@gmail.com>
2023-01-10 15:46:27 +0700

    up

--- Soal01.java
+++ Soal01.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Soal01 {
 
 	public static void main(final String[] args) {
-		// Kerjakan soalnya di sini
+		
+
 	}
+	
 }
--- Soal02.java
+++ Soal02.java
@@ -1,6 +1,14 @@
+import java.util.*;
 public class Soal02 {
 
 	public static void main(final String[] args) {
-		// Kerjakan soalnya di sini
-	}
-}
+		Scanner scan = new Scanner(System.in);
+		String vocal = scan.nextLine();
+		String text;
+		text = scan.next();
+		String replace = text.replaceAll(" ",vocal + " ");
+		System.out.println(replace);
+		scan.close();
+		}
+		}
+
